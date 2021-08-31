0465ee7860d53fbe178eba9036f9538cd3e5dd8c
GPA0VERKD (laptop) <hungthui@outlook.com>
2021-08-31 16:25:39 -0700

    added merge sort algorithm

---
+++ Sorting_Algorithms/Merge_Sort.py
@@ -0,0 +1,41 @@
+def mergeSort(array: list):
+    if len(array) > 1:
+        # split array into two sides
+        mid = len(array) // 2
+        arr1 = array[:mid]
+        arr2 = array[mid:]
+
+        # recursive call on left and right side
+        mergeSort(arr1)
+        mergeSort(arr2) 
+
+        # tracks indeces for arr1, arr2, and array respectively
+        i = j = k = 0
+
+        # copy data to temp arrays, arr1 and arr2
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i] < arr2[j]:
+                array[k] = arr1[i]
+                i += 1
+            else:
+                array[k] = arr2[j]
+                j += 1
+            k += 1
+        
+        # sorts remaining elements of longer list
+        while i < len(arr1):
+            array[k] = arr1[i]
+            i += 1
+            k += 1
+
+        while j < len(arr2):
+            array[k] = arr2[j]
+            j += 1
+            k += 1
+
+
+if __name__ == '__main__':
+    a = [12, 11, 13, 5, 6, 7]
+    print(f"Given Array: {a}")
+    mergeSort(a)
+    print(f"Sorted Array: {a}")
